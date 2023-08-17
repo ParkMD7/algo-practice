0a02f2889ee8586bd9f1b671413a46dfb5dd888f
Parker <parkermdinsmore@gmail.com>
2023-08-17 10:54:38 -0400

    practice queues

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -14,61 +14,6 @@
 //     [12, 13, 14, 5],
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
-function matrix(n) {
-  let results = [];
-
-  for (let i = 0; i < n; i++) {
-    results.push([]);
-  }
-
-  let counter = 1;
-  let startRow = 0;
-  let endRow = n - 1;
-  let startColumn = 0;
-  let endColumn = n - 1;
-
-  while (startRow <= endRow && startColumn <= endColumn) {
-    // TOP ROW
-    // iterate through startRow and assign n values
-    // then increment the startRow (aka move it down)
-    for (let i = startColumn; i <= endColumn; i++) {
-      results[startRow][i] = counter;
-      counter++
-    }
-    startRow++ 
-
-    // RIGHT SIDE
-    // iterate from startRow to endRow and assign n values
-    // then decrement the endColumn (aka move it left)
-    for (let i = startRow; i <= endRow; i++) {
-      console.log('i', i)
-      results[i][endColumn] = counter;
-      counter++
-    }
-    endColumn--
-
-    // BOTTOM ROW
-    // reverse iterate through endRow and assign n values
-    // then decrement the endRow (aka move it up)
-    for (let i = endColumn; i >= startColumn; i--) {
-      results[endRow][i] = counter;
-      counter++ 
-    }
-    endRow--
-
-    // LEFT SIDE
-    // reverse iterate through startColumn and assign n values
-    // then increment the startColumn (aka move it right)
-    for (let i = endRow; i >= startRow; i--) {
-      results[i][startColumn] = counter
-      counter++
-    }
-    startColumn++
-    console.log('results', results)
-  }
-
-  // console.log('results', results)
-  return results;
-}
+function matrix(n) {}
 
 module.exports = matrix;
