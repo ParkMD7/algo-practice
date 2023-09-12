f2b6b6f90fe0510cbaf60efb4c59114980386391
Parker <parkermdinsmore@gmail.com>
2023-09-12 14:36:23 -0700

    practice chunk

--- exercises/chunk/completed.js
+++ exercises/chunk/completed.js
@@ -8,44 +8,42 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-
 // one solution
-// function chunk(array, size) {
-//   let chunked = [];
-
-//   for (let el of array) {
-//     // check for last element in chunked array
-//     const lastElementOfChunked = chunked[chunked.length - 1];
-//     console.log('lastElementOfChunked', lastElementOfChunked)
-
-//     // if the last element doesnt exist OR its length equals the size of the array then push a new array into chunked
-//     if (!lastElementOfChunked || lastElementOfChunked.length === size) {
-//       chunked.push([el])
-//     } else {
-//       lastElementOfChunked.push(el)
-//     }
-//   }
-
-//   return chunked;
-// }
-
-// second solution
 function chunk(array, size) {
   let chunked = [];
 
-  array.forEach((el) => {
+  for (let el of array) {
     // check for last element in chunked array
-    const lastElInChunked = chunked[chunked.length - 1];
+    const chunk = chunked[chunked.length - 1];
 
     // if the last element doesnt exist OR its length equals the size of the array then push a new array into chunked
-    if (!lastElInChunked || lastElInChunked.length === size) {
+    if (!chunk || chunk.length === size) {
       chunked.push([el]);
     } else {
-      lastElInChunked.push(el);
+      chunk.push(el);
     }
-  });
+  }
 
   return chunked;
 }
+
+// second solution
+// function chunk(array, size) {
+//   let chunked = [];
+
+//   array.forEach((el) => {
+//     // check for last element in chunked array
+//     const lastElInChunked = chunked[chunked.length - 1];
+
+//     // if the last element doesnt exist OR its length equals the size of the array then push a new array into chunked
+//     if (!lastElInChunked || lastElInChunked.length === size) {
+//       chunked.push([el]);
+//     } else {
+//       lastElInChunked.push(el);
+//     }
+//   });
+
+//   return chunked;
+// }
 
 module.exports = chunk;
