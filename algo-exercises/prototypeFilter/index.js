6828aa923971a83572d499eb2e3678ee537d8153
Parker <parkermdinsmore@gmail.com>
2023-10-05 13:48:51 -0400

    filter prototype question

---
+++ algo-exercises/prototypeFilter/index.js
@@ -0,0 +1,28 @@
+// --- Directions
+// Array.prototype.filter creates a new array populated with the results of calling a provided function on every element in the calling array.
+
+// Implement Array.prototype.filter. 
+// To avoid overwriting the actual Array.prototype.filter instead implement it as Array.prototype.myFilter.
+
+// make sure to add args to the function
+Array.prototype.myFilter = function (callbackFn, thisArg) {
+  // length of array arg being called with custom myFilter
+  const len = this.length;
+  const results = [];
+
+  for (let i = 0; i < len; i++) {
+    // Cache value in case the callback function modifies it.
+    const cachedI = this[i];
+    if (
+      // Ignore index if value is not defined for index (e.g. in sparse arrays).
+      Object.hasOwn(this, i) &&
+      callbackFn.call(thisArg, cachedI, i, this)
+    ) {
+      results.push(cachedI);
+    }
+  }
+
+  return results;
+};
+
+module.exports = Array.prototype.myFilter;
