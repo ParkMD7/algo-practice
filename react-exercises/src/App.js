71be61c74bbe0f3a03212076b71779ef858d613a
Parker <parkermdinsmore@gmail.com>
2023-10-06 16:59:02 -0400

    practice with tables

--- react-exercises/src/App.js
+++ react-exercises/src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 
 // Accordion
 // import Problem from "./problems/Accordion/Problem";
-import Accordion from "./problems/Accordion/solution/Accordion";
+// import Accordion from "./problems/Accordion/solution/Accordion";
+
+// Table
+// import Problem from "./problems/Table/Problem";
+import Table from "./problems/Table/solution/Table";
 
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <Accordion />
+      <Table />
     </div>
   );
 }
---
+++ react-exercises/src/problems/Table/solution/Table.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+
+const Table = () => {
+  const [rows, setRows] = useState(1);
+  const [columns, setColumns] = useState(1);
+  const [showTable, setShowTable] = useState(false);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setShowTable(true);
+    console.log("clicked");
+  };
+
+  const handleSetRows = (e) => {
+    setShowTable(false);
+    setRows(e.target.value);
+  };
+
+  const handleSetColumns = (e) => {
+    setShowTable(false);
+    setColumns(e.target.value);
+  };
+
+  const renderTableInputForm = () => {
+    return (
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="rows">Rows</label>
+        <input
+          value={rows}
+          onChange={handleSetRows}
+          type="number"
+          min={1}
+          id="rows"
+          name="rows"
+        />
+        <br />
+        <label htmlFor="columns">Columns</label>
+        <input
+          value={columns}
+          onChange={handleSetColumns}
+          type="number"
+          min={1}
+          id="columns"
+          name="columns"
+        />
+        <br />
+        <button onClick={handleFormSubmit}>Submit</button>
+      </form>
+    );
+  };
+
+  const renderTable = () => {
+    return (
+      <tbody>
+        {Array.from({ length: rows }, () => 0).map((_, row) => (
+          <tr key={row}>
+            {Array.from({ length: columns }, () => 0).map((_, col) => (
+              <td key={col}>
+                {col % 2 === 0
+                  ? rows * col + (row + 1)
+                  : rows * (col + 1) - row}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    );
+  };
+
+  return (
+    <div>
+      {renderTableInputForm()}
+      <br />
+      <div>{showTable && <table>{renderTable()}</table>}</div>
+    </div>
+  );
+};
+
+export default Table;
