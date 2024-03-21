e2c6ac64de96b87f04cd101ffa36416357e2aad2
Parker <parkermdinsmore@gmail.com>
2024-03-20 23:55:32 -0400

    add new problem for reversing linked list

---
+++ algo-exercises/linked-lists/reversedList/index.js
@@ -0,0 +1,24 @@
+// --- Directions
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+
+// --- Example
+//   const l = new LinkedList();
+//   l.insertLast(1)
+//   l.insertLast(2)
+//   l.insertLast(3)
+//   l.insertLast(4)
+//   l.insertLast(5)
+//   reversedList(l); // returns [5, 4, 3, 2, 1]
+
+function reversedList(list) {}
+
+module.exports = reversedList;
---
+++ algo-exercises/linked-lists/reversedList/test.js
@@ -0,0 +1,29 @@
+const reversedList = require('./index');
+const L = require('./linkedlist');
+const LinkedList = L.LinkedList;
+
+test('reversedList is a function', () => {
+  expect(typeof reversedList).toEqual('function');
+});
+
+  test('when the list has 2 elements', () => {
+    const l = new LinkedList();
+    l.insertLast(1);
+    l.insertLast(2);
+    expect(reversedList(l).data).toEqual([2, 1]);
+  });
+
+  test('when the list has 5 elements', () => {
+    const l = new LinkedList();
+    l.insertLast(1);
+    l.insertLast(2);
+    l.insertLast(3);
+    l.insertLast(4);
+    l.insertLast(5);
+    expect(reversedList(l).data).toEqual([5, 4, 3, 2, 1]);
+  });
+
+  test('when the list has 0 elements', () => {
+    const l = new LinkedList();
+    expect(reversedList(l).data).toEqual([]);
+  });
