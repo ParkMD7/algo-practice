1c15e4b2195a78c5f330a978af0e22c302c929df
Parker <parkermdinsmore@gmail.com>
2024-04-11 11:55:38 -0700

    practice with find first palindrome

---
+++ algo-exercises/strings-and-numbers/firstPalindrome/index.js
@@ -0,0 +1,26 @@
+// --- Directions
+// Given an array of strings words, return the first palindromic string in the array.
+// If there is no such string, return an empty string "".
+// A string is palindromic if it reads the same forward and backward.
+
+// --- Examples:
+//   firstPalindrome(["abc","car","ada","racecar","cool"]) === "ada"
+//   firstPalindrome(["notapalindrome","racecar"]) === "racecar"
+//   firstPalindrome(["def","ghi"]) === ""
+
+// to run tests from the algo-exercises directory: `jest strings-and-numbers/firstPalindrome/test.js --watch`
+
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+
+const checkIsPalindrome = (word) => {
+  return word === word.split("").reverse().join("");
+}
+
+function firstPalindrome(words) {
+  return words.find((word) => checkIsPalindrome(word)) || "";
+}
+
+module.exports = firstPalindrome;
